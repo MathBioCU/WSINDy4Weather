d650b7d7a308c0219082a3eaa0791d9cfb5a225f
Seth Minor <97004318+SethMinor@users.noreply.github.com>
2025-06-28 16:45:45 -0600

    Create plot_settings.py

---
+++ plot_settings.py
@@ -0,0 +1,8 @@
+# Plot settings
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams['font.size'] = 12
+plt.rcParams['figure.dpi'] = 100
+
+matplotlib.rc('xtick', labelsize=9, direction='out')
+matplotlib.rc('ytick', labelsize=9, direction='out')
